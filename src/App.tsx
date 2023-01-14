20957745e972840d8c19dd50dc0051e760663aa0
Shayan Mahtabi <shayan.mhtb@gmail.com>
2023-01-14 23:12:33 +0100

    App file is updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
 
-import Header from './components/Header';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
     </div>
   );
 }

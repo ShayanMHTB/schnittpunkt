b7f43b251d51f25024a2bca576dfb00da04fb224
Shayan Mahtabi <shayan.mhtb@gmail.com>
2023-01-14 22:22:04 +0100

    App file is updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <hr />
-      <div className='m-2 p-5 flex items-center justify-center'>
-        <h1 className='text-2xl bg-red-400 p-10 rounded-2xl text-gray-200 hover:text-black'>
-          Hello World! React + Tailwind
-        </h1>
-      </div>
+      <Header></Header>
     </div>
   );
 }

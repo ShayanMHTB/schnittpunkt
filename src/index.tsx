03ae325db510f10d455b10ff5055b6646bba27ec
Shayan Mahtabi <shayan.mhtb@gmail.com>
2023-01-14 23:12:47 +0100

    Index file is updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
+
 import './index.scss';
+
+import Header from './components/Header';
+
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +15,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    {/* <App /> */}
   </React.StrictMode>
 );
 

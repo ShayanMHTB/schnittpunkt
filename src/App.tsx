ed24ee025d0acd18babbf7a749fbccb01eaca581
Shayan Mahtabi <shayan.mhtb@gmail.com>
2023-01-15 01:40:19 +0100

    App files updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex items-center justify-center">
+      <h1 className='text-2xl'>Schnittpunkt</h1>
     </div>
   );
 }

6135cffc547fa170d803f6816460ea925f656f12
Shayan Mahtabi <shayan.mhtb@gmail.com>
2023-01-15 01:39:03 +0100

    Header component updated according to recent changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,72 +3,32 @@ import React from "react";
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
-import { FiMenu } from "react-icons/fi";
-import { BsFillCircleFill } from "react-icons/bs";
+import App from "../App";
+import Imprint from "../pages/Imprint";
+import NotFound from "../pages/NotFound";
 
-import Home from "../pages/Home";
-import Price from "../pages/Price";
-import Gallery from "../pages/Gallery";
-import Contact from "../pages/Contact";
+import { navbarItems } from "../data/NavbarItems";
+
+import Navbar from "./Navbar";
 
 export default function Header() {
-  const navbarItems = [
-    {
-      id: "home",
-      name: "HOME",
-      path: "/home",
-      element: Home
-    },
-    {
-      id: "price",
-      name: "PREISE",
-      path: "/price",
-      element: Price
-    },
-    {
-      id: "gallery",
-      name: "GALERIE",
-      path: "/gallery",
-      element: Gallery
-    },
-    {
-      id: "contact",
-      name: "KONTAKT",
-      path: "/contact",
-      element: Contact
-    }
-  ]
 
   return (
     <Router>
-      <nav className="navbar fixed top-0 left-0 flex w-screen px-4 my-4">
-        <Link to={"/"} className="navbar-logo flex items-center justify-between">
-          SCHNITT <BsFillCircleFill className="ml-2" color="red" />
-        </Link>
-        <button className="navbar-toggler flex sm:hidden items-center justify-center p-2 ml-auto">
-          <FiMenu size={20} />
-        </button>
-        <ul className="navbar-items hidden sm:flex items-center justify-center ml-auto">
-          {navbarItems.map((navbarItem) => {
-            return (
-              <li className="p-2 mx-2">
-                <Link to={navbarItem.path}>{navbarItem.name}</Link>
-              </li>
-            )
-          })}
-        </ul>
-      </nav>
+      <Navbar />
       <Routes>
         { navbarItems.map((navbarItem) => {
           const Element = navbarItem.element;
           return (
-            <Route path={navbarItem.path} element={ <Element /> }></Route>
+            <Route key={navbarItem.id} path={navbarItem.path} element={ <Element /> }></Route>
           )
         }) }
+        <Route path="/" element={ <App /> }></Route>
+        <Route path="/imprint" element={ <Imprint /> }></Route>
+        <Route path="/*" element={ <NotFound /> }></Route>
       </Routes>
     </Router>
   );
